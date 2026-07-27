deddb205ce1522b8087ee00b8c85c0cc63640153
scoutingsampdoria2026 <scouting.sampdoria@gmail.com>
2026-07-27 09:21:53 +0200

    Update ProvinoDetailScreen.kt

--- app/src/main/java/com/scoutingsampdoria/persone2/ui/screens/ProvinoDetailScreen.kt
+++ app/src/main/java/com/scoutingsampdoria/persone2/ui/screens/ProvinoDetailScreen.kt
@@ -412,9 +412,3 @@ private fun SelettoreGiudizio(valore: Int?, onValoreCambiato: (Int?) -> Unit) {
     }
 }
 
-/** Trasforma "2026-07-24" in "24/07/2026" */
-private fun formattaDataItaliana(data: String?): String {
-    if (data.isNullOrBlank()) return "-"
-    val parti = data.split("-")
-    return if (parti.size == 3) "${parti[2]}/${parti[1]}/${parti[0]}" else data
-}
